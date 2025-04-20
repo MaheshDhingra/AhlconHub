b9dfcc0638fba1759e743a9dadefe5cac33a3f64
jmrkd-studio <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-04-20 10:07:50 +0530

    --fixed-prisma

--- ahlconhub/app/layout.tsx
+++ ahlconhub/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import Header from '@/components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -22,7 +21,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={roboto.className}>
-          <Header />
           <main className='container'>{children}</main>
           <ToastContainer />
         </body>
--- ahlconhub/app/page.tsx
+++ ahlconhub/app/page.tsx
@@ -1,24 +1,15 @@
 import { currentUser } from '@clerk/nextjs/server';
-import Guest from '@/components/Guest';
-import AddTransaction from '@/components/AddTransaction';
-import Balance from '@/components/Balance';
-import IncomeExpense from '@/components/IncomeExpense';
-import TransactionList from '@/components/TransactionList';
 
 const HomePage = async () => {
   const user = await currentUser();
 
   if (!user) {
-    return <Guest />;
+    return console.error('User not found');
   }
 
   return (
     <main>
       <h2>Welcome, {user.firstName}</h2>
-      <Balance />
-      <IncomeExpense />
-      <AddTransaction />
-      <TransactionList />
     </main>
   );
 };
